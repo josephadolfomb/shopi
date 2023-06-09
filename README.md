79100afb9ff3e90550423902e6dce60335d7298f
josephadolfomb <josephmobu@gmail.com>
2023-06-08 21:55:52 -0500

    project start

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shopi
+
+This project is an E-Commerce Website created with React, Vite & TailwindCSS. It is a sample of my portfolio.
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='bg-emerald-600'>
+      Hola mundo.
+    </div>
+  )
+}
+
+export default App
