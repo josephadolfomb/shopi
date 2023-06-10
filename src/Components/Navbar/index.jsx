f0b6f3b5315461844470b5af328cde9b2459cba0
josephadolfomb <josephmobu@gmail.com>
2023-06-10 16:50:34 -0500

    email update

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
       </ul>
       <ul className='flex items-center gap-3'>
         <li className='text-black/60'>
-          teff@platzi.com
+          joseph@email.com
         </li>
         <li>
           <NavLink
