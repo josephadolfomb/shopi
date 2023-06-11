508593a4480291daadab7512d4d0c565c714be58
josephadolfomb <josephmobu@gmail.com>
2023-06-10 22:37:47 -0500

    handle sign in logic

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState, useRef } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 import { ShoppingCartContext } from '../../Context'
 import Layout from '../../Components/Layout'
 
@@ -7,7 +7,6 @@ function SignIn() {
   const context = useContext(ShoppingCartContext)
   const [view, setView] = useState('user-info')
   const form = useRef(null)
-
   // Account
   const account = localStorage.getItem('account')
   const parsedAccount = JSON.parse(account)
@@ -16,6 +15,14 @@ function SignIn() {
   const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true
   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
 
+  const handleSignIn = () => {
+    const stringifiedSignOut = JSON.stringify(false)
+    localStorage.setItem('sign-out', stringifiedSignOut)
+    context.setSignOut(false)
+    // Redirect
+    return <Navigate replace to={'/'} />
+  }
+
   const createAnAccount = () => {
 		const formData = new FormData(form.current)
 		const data = {
@@ -23,8 +30,12 @@ function SignIn() {
 			email: formData.get('email'),
 			password: formData.get('password')
 		}
-    // TODO: Remove this console.log 🥲
-		console.log(data)
+    // Create account
+    const stringifiedAccount = JSON.stringify(data)
+    localStorage.setItem('account', stringifiedAccount)
+    context.setAccount(data)
+    // Sign In
+    handleSignIn()
 	}
 
   const renderLogIn = () => {
@@ -59,7 +70,6 @@ function SignIn() {
       </div>
     )
   }
-
   const renderCreateUserInfo = () => {
     return (
       <form ref={form} className='flex flex-col gap-4 w-80'>
@@ -109,7 +119,6 @@ function SignIn() {
       </form>
     )
   }
-
   const renderView = () => view === 'create-user-info' ? renderCreateUserInfo() : renderLogIn()
   return (
     <Layout>
